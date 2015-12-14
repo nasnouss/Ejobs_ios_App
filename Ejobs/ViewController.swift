cc4b7a0591bc53186b7da508955b2dc84312a77d
anis ben brahim <anissou16@hotamail.com>
2015-12-14 14:01:32 +0100

    Initial Commit

---
+++ Ejobs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ejobs
+//
+//  Created by anis ben brahim on 14/12/2015.
+//  Copyright © 2015 anis ben brahim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
